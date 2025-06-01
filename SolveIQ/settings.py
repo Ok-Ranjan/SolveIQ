13fd654fa671b16bb1c96db9bc47d3aa299a35d2
Ranjan <ranjannp802114@gmail.com>
2025-06-02 04:00:56 +0530

    change setting.py

--- SolveIQ/settings.py
+++ SolveIQ/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-at*9*exy3k)28_wzp8swgdpmxpw%ini6hl@6kv6_k5d&-pjx-d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SolveIQ.com', '127.0.0.1']
 
 
 # Application definition
