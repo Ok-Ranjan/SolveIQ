00c30ba9ce8a99b39868442028bd1cd5a4562919
Ranjan <ranjannp802114@gmail.com>
2025-06-02 09:05:06 +0530

    update setting.py for deploying on Koyeb

--- SolveIQ/settings.py
+++ SolveIQ/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-at*9*exy3k)28_wzp8swgdpmxpw%ini6hl@6kv6_k5d&-pjx-d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['SolveIQ.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['SolveIQ.koyeb.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -117,7 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
